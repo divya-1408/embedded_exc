d4146024adc55edae0d4a86a76920ce2c81137f5
divya-1408 <123148210+divya-1408@users.noreply.github.com>
2023-06-20 17:42:32 +0530

    Create LED_blink_with_ORlogic.c

---
+++ LED_blink_with_ORlogic.c
@@ -0,0 +1,19 @@
+#include <AT89S52.h>
+#define LED P2_4
+#define ON 0
+#define OFF 1
+#define SW1 P3_3
+#define SW2 P2_3
+#define INPUT 1
+void main()
+{
+   SW1 = INPUT;
+   SW2 = INPUT;
+   LED = OFF;
+   while(1){
+   __bit a, b;
+   a=!SW1;
+   b=!SW2;
+   LED = !(a|b);
+   }
+}
